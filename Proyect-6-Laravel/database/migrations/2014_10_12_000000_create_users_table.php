c24cbf1a50af68d47eb215bfe67e726ae0fb5a07
alvarito101093 <alvaro101093@gmail.com>
2023-04-05 18:13:57 +0200

    feat: users and roles migrations rows completed apparently

--- Proyect-6-Laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ Proyect-6-Laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname');
+            $table->string('nickname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('direction');
+            $table->date('birth_date');
             $table->rememberToken();
             $table->timestamps();
         });
