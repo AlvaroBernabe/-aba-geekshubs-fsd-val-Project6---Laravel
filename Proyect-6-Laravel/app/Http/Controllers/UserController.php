359804fe6da87d415cc767d7189c9cf4a9fedb1d
alvarito101093 <alvaro101093@gmail.com>
2023-04-12 23:05:40 +0200

    feat: UserController Cleaned Code

--- Proyect-6-Laravel/app/Http/Controllers/UserController.php
+++ Proyect-6-Laravel/app/Http/Controllers/UserController.php
@@ -2,25 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Game;
-use App\Models\Message;
-use App\Models\Party;
 use App\Models\User;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use Laravel\Sanctum\PersonalAccessToken;
 
 use function PHPUnit\Framework\isNull;
 
 class UserController extends Controller
 {
 
-    public function updateprofile(Request $request){
-    {
+    public function updateprofile(Request $request)
+    { {
             try {
+                Log::info("Update Profile Working");
                 $id = auth()->user()->id;
                 $id2 = DB::table('users')->where('id', '=', $id)->get();
                 $validator = Validator::make($request->all(), [
@@ -43,37 +40,37 @@ class UserController extends Controller
                         404
                     );
                 }
-            $name = $request->input('name');
-            $surname = $request->input('surname');
-            $nickname = $request->input('nickname');
-            $phone_number = $request->input('phone_number');
-            $direction = $request->input('direction');
-            $age = $request->input('age');
-            if (isNull($name, $surname, $nickname, $phone_number, $direction, $age)) {
-                $user->name = $name;
-                $user->surname = $surname;
-                $user->nickname = $nickname;
-                $user->phone_number = $phone_number;
-                $user->direction = $direction;
-                $user->age = $age;
-            }
-            $user->save();
-            return response()->json(
-                [
-                    "success" => true,
-                    "message" => "Profile Updated Correctly",
-                    "data" => $user
-                ],
-                200
-            );
+                $name = $request->input('name');
+                $surname = $request->input('surname');
+                $nickname = $request->input('nickname');
+                $phone_number = $request->input('phone_number');
+                $direction = $request->input('direction');
+                $age = $request->input('age');
+                if (isNull($name, $surname, $nickname, $phone_number, $direction, $age)) {
+                    $user->name = $name;
+                    $user->surname = $surname;
+                    $user->nickname = $nickname;
+                    $user->phone_number = $phone_number;
+                    $user->direction = $direction;
+                    $user->age = $age;
+                }
+                $user->save();
+                return response()->json(
+                    [
+                        "success" => true,
+                        "message" => "Profile Updated Correctly",
+                        "data" => $user
+                    ],
+                    200
+                );
             } catch (\Throwable $th) {
                 Log::error("Update Profile error: " . $th->getMessage());
                 return response()->json(
                     [
                         "success" => false,
-                        "message" => "Update Profile error ". ($user)
+                        "message" => $th->getMessage()
                     ],
-                    Response::HTTP_INTERNAL_SERVER_ERROR
+                    500
                 );
             }
         }
@@ -82,8 +79,8 @@ class UserController extends Controller
     public function myProfile()
     {
         try {
+            Log::info("Get My Profile Working");
             $user = auth()->user();
-
             return response(
                 [
                     "success" => true,
@@ -97,36 +94,28 @@ class UserController extends Controller
             return response()->json(
                 [
                     "success" => false,
-                    "message" => "Cant Get my profile error"
+                    "message" => $th->getMessage()
                 ],
-                Response::HTTP_INTERNAL_SERVER_ERROR
+                500
             );
         }
     }
 
-
-
-
-
-
-
-
-
-
-
     public function getAllUsers()
     {
         try {
+            Log::info("Get All Users Working");
             $users = User::query()->get();
             return [
                 "success" => true,
                 "data" => $users
             ];
         } catch (\Throwable $th) {
+            Log::error("Get All Users error: " . $th->getMessage());
             return response()->json(
                 [
                     "success" => false,
-                    "message" => $th->getMessage() . $users
+                    "message" => $th->getMessage()
                 ],
                 500
             );
@@ -136,6 +125,7 @@ class UserController extends Controller
     public function getUserDetailsById(Request $request, $id)
     {
         try {
+            Log::info("Get User Details By Id Working");
             $users = User::query()->find($id);
             return response()->json(
                 [
@@ -154,6 +144,7 @@ class UserController extends Controller
                 200
             );
         } catch (\Throwable $th) {
+            Log::error("Get User Details By Id error: " . $th->getMessage());
             return response()->json(
                 [
                     "success" => false,
@@ -164,69 +155,32 @@ class UserController extends Controller
         }
     }
 
-        public function deleteUserById(Request $request, $id)
+    public function deleteUserById(Request $request, $id)
     {
         try {
-        //     // $id = auth()->user()->id;
-        //     $id2 = DB::table('users')->where('role_id', '=', 1)->get('role_id', '=', 1);
-        //     // $userId = auth()->user();
-        //     // $user = User::find($userId);
-        //     $id2 = [
-        //         'array' => 'integer'
-        //         ];
-        //     $test = (int)$id2;
-        // //     if (sayHello() === FALSE)
-        // //     echo "Function Failed";
-        // // else
-        // //     echo "Function Worked";
-        // //     User::destroy($id) 
-
-        //             if($test != 1) {
-        //                 return response()->json([
-        //                     'success' => true,
-        //                     'message' => "You cant delete Yourself",
-        //                     "message" => $test . "esto es test"
-        //                 ]);
-        //               } else {
-        //                 User::destroy($id);
-        //                 }
-        //     return response()->json(
-        //         [
-        //             "success" => true,
-        //             "message" => "User Deleted",
-        //             "message" => $test . "esto es test"
-        //         ],
-        //         200
-        //     );
-
-        $user = User::find($id);
-        if ($user->role_id != 1) {
-            User::destroy($id);
-            return response()->json([
-                'success' => true,
-                'message' => 'User successfully deleted',
-            ], 200);
-        } else {
-            return response()->json([
-                'success' => false,
-                'message' => 'You cant delete Yourself or Other Admin'
-            ], 400);
-        }
-
-
+            Log::info("Delete User By Id Working");
+            $user = User::find($id);
+            if ($user->role_id != 1) {
+                User::destroy($id);
+                return response()->json([
+                    'success' => true,
+                    'message' => 'User successfully deleted',
+                ], 200);
+            } else {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'You cant delete Yourself or Other Admin'
+                ], 400);
+            }
         } catch (\Throwable $th) {
+            Log::error("Delete User By Id error: " . $th->getMessage());
             return response()->json(
                 [
                     "success" => false,
-                    "message" => $th->getMessage() 
+                    "message" => $th->getMessage()
                 ],
                 500
             );
         }
     }
-
-
-
-
-
 }
